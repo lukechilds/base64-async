fb49009535a261f8cfd6a44fd4352c185fe42c63
Luke Childs <lukechilds123@gmail.com>
2017-05-19 12:03:33 +0700

    Test b64.encode encodes buffer to Base64

--- test/b64.encode.js
+++ test/b64.encode.js
@@ -9,3 +9,12 @@ test('b64.encode encodes string to Base64', async t => {
 
 	t.is(b64String, expectedB64String);
 });
+
+test('b64.encode encodes buffer to Base64', async t => {
+	const buffer = Buffer.from('This is a test string');
+	const expectedB64String = buffer.toString('base64');
+
+	const b64String = await b64.encode(buffer);
+
+	t.is(b64String, expectedB64String);
+});
