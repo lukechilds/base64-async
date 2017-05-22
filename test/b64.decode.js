f0241d308e1bbdb1ffb882191f0495262af06fbc
Luke Childs <lukechilds123@gmail.com>
2017-05-22 17:07:26 +0700

    Test b64.decode decodes Base64 to buffer

--- test/b64.decode.js
+++ test/b64.decode.js
@@ -6,3 +6,9 @@ test('b64.decode returns a Promise', t => {
 	const returnValue = b64.decode(values.string);
 	t.true(returnValue instanceof Promise);
 });
+
+test('b64.decode decodes Base64 to buffer', async t => {
+	const result = await b64.decode(values.base64);
+	t.true(result instanceof Buffer);
+	t.is(result.toString(), values.string);
+});
