6584a037f09b832c8f12b1b1dccf6d90b8a84fd7
Luke Childs <lukechilds123@gmail.com>
2017-05-24 17:37:23 +0700

    Use non utf8 buffer to help catch decoding errors e.g When using a utf8 string buffer we could store the buffer as a utf8 string and tests would pass. If you then tried to encode/decode an image you'd get incorrect results.

--- test/fixtures/values.js
+++ test/fixtures/values.js
@@ -1,4 +1,4 @@
-const buffer = Buffer.from('This is a test string');
+const buffer = Buffer.from([0x72, 0x98, 0x4f, 0x96, 0x75, 0x03, 0xcd, 0x28, 0x38, 0xbf]);
 const base64 = buffer.toString('base64');
 const hexString = buffer.toString('hex');
 
