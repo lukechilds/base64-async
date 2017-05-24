8afa270cf91509a5dd079b3686a457ded694bc14
Luke Childs <lukechilds123@gmail.com>
2017-05-24 17:33:50 +0700

    Remove string export from fixture values

--- test/fixtures/values.js
+++ test/fixtures/values.js
@@ -1,10 +1,8 @@
-const string = 'This is a test string';
-const buffer = Buffer.from(string);
+const buffer = Buffer.from('This is a test string');
 const base64 = buffer.toString('base64');
 const hexString = buffer.toString('hex');
 
 module.exports = {
-  string,
   buffer,
   base64,
   hexString
