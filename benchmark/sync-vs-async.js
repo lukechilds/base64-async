5691257655497b696e2c835d06faf8d525f722d1
Luke Childs <lukechilds123@gmail.com>
2017-05-24 16:07:37 +0700

    Keep xo happy

--- benchmark/sync-vs-async.js
+++ benchmark/sync-vs-async.js
@@ -1,68 +1,70 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-await-in-loop, no-return-assign */
+
 const crypto = require('crypto');
-const b64 = require('../');
 const prettyBytes = require('pretty-bytes');
 const Table = require('cli-table');
+const b64 = require('../');
 
 const bytesToBenchmark = [10000, 100000, 1000000, 10000000];
 
 const timer = {
-  reset: () => timer.startTime = process.hrtime(),
-  duration: () => process.hrtime(timer.startTime)[1] / 1000000
+	reset: () => timer.startTime = process.hrtime(),
+	duration: () => process.hrtime(timer.startTime)[1] / 1000000
 };
 
 const bench = noOfBytes => Promise.resolve().then(async () => {
-  const results = {};
+	const results = {};
 
-  console.log(`Generating ${prettyBytes(noOfBytes)} of random binary data...`);
-  const randomBytes = crypto.randomBytes(noOfBytes);
+	console.log(`Generating ${prettyBytes(noOfBytes)} of random binary data...`);
+	const randomBytes = crypto.randomBytes(noOfBytes);
 
-  console.log('Encoding sync...');
-  timer.reset();
-  const randomBytesBase64 = randomBytes.toString('base64');
-  results.encodeSync = timer.duration();
+	console.log('Encoding sync...');
+	timer.reset();
+	const randomBytesBase64 = randomBytes.toString('base64');
+	results.encodeSync = timer.duration();
 
-  console.log('Decoding sync...');
-  timer.reset();
-  Buffer.from(randomBytesBase64, 'base64');
-  results.decodeSync = timer.duration();
+	console.log('Decoding sync...');
+	timer.reset();
+	Buffer.from(randomBytesBase64, 'base64');
+	results.decodeSync = timer.duration();
 
-  console.log('Encoding async...');
-  timer.reset();
-  await b64(randomBytes);
-  results.encodeAsync = timer.duration();
+	console.log('Encoding async...');
+	timer.reset();
+	await b64(randomBytes);
+	results.encodeAsync = timer.duration();
 
-  console.log('Decoding async...');
-  timer.reset();
-  await b64(randomBytesBase64);
-  results.decodeAsync = timer.duration();
+	console.log('Decoding async...');
+	timer.reset();
+	await b64(randomBytesBase64);
+	results.decodeAsync = timer.duration();
 
-  console.log();
-  return results;
+	console.log();
+	return results;
 });
 
 (async () => {
-  const table = new Table({
-    head: [
-      'Bytes',
-      'Encode Sync',
-      'Decode Sync',
-      'Encode Async',
-      'Decode Async'
-    ]
-  });
+	const table = new Table({
+		head: [
+			'Bytes',
+			'Encode Sync',
+			'Decode Sync',
+			'Encode Async',
+			'Decode Async'
+		]
+	});
 
-  for(noOfBytes of bytesToBenchmark) {
-    const results = await bench(noOfBytes);
-    table.push([
-      prettyBytes(noOfBytes),
-      results.encodeSync + 'ms',
-      results.decodeSync + 'ms',
-      results.encodeAsync + 'ms',
-      results.decodeAsync + 'ms'
-    ]);
-  }
+	for (const noOfBytes of bytesToBenchmark) {
+		const results = await bench(noOfBytes);
+		table.push([
+			prettyBytes(noOfBytes),
+			results.encodeSync + 'ms',
+			results.decodeSync + 'ms',
+			results.encodeAsync + 'ms',
+			results.decodeAsync + 'ms'
+		]);
+	}
 
-  console.log(table.toString());
+	console.log(table.toString());
 })();
