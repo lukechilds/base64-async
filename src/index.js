05ccc0f6893083587f16ea0d21c6b2a959929ba6
Luke Childs <lukechilds123@gmail.com>
2017-05-28 22:12:34 +0700

    Decode base64 in pure JavaScript So we don't have to use expensive buffers

--- src/index.js
+++ src/index.js
@@ -53,18 +53,52 @@ b64.decode = (input, opts) => new Promise(resolve => {
 		throw new TypeError('input must be a base64 string');
 	}
 
+	const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/-_';
+	const lookup = Buffer.from(alphabet);
+	const maxCharCode = Math.max(...Array.from(lookup));
+	const revLookup = Buffer.allocUnsafe(maxCharCode + 1);
+	lookup.forEach((v, i) => {
+		revLookup[v] = i;
+	});
+	// Support decoding url safe b64
+	revLookup['-'.charCodeAt(0)] = 62;
+	revLookup['_'.charCodeAt(0)] = 63;
+
 	const stringLength = input.length;
-	const outputBuffers = [];
-	let currentIndex = 0;
+	const placeHolders = input[stringLength - 2] === '=' ? 2 : input[stringLength - 1] === '=' ? 1 : 0;
+	const byteLength = (stringLength * 3 / 4) - placeHolders;
+	const output = Buffer.allocUnsafe(byteLength);
+	const stringLengthFullChunks = placeHolders > 0 ? stringLength - 4 : stringLength;
+
+	var stringIndex = 0;
+	var byteIndex = 0;
 
 	setImmediate(function decodeChunk() {
-		const chunk = input.slice(currentIndex, currentIndex + opts.chunkSize);
-		outputBuffers.push(Buffer.from(chunk, 'base64'));
-		currentIndex += opts.chunkSize;
-		if (currentIndex < stringLength) {
+		const chunkEndIndex = Math.min(stringIndex + opts.chunkSize, stringLengthFullChunks);
+
+		while (stringIndex < chunkEndIndex) {
+			const tmp = (revLookup[input.charCodeAt(stringIndex)] << 18) | (revLookup[input.charCodeAt(stringIndex + 1)] << 12) | (revLookup[input.charCodeAt(stringIndex + 2)] << 6) | revLookup[input.charCodeAt(stringIndex + 3)];
+			output[byteIndex++] = (tmp >> 16) & 0xFF;
+			output[byteIndex++] = (tmp >> 8) & 0xFF;
+			output[byteIndex++] = tmp & 0xFF;
+			stringIndex += 4;
+		}
+
+		if (stringIndex === stringLengthFullChunks) {
+			if (placeHolders === 2) {
+				const tmp = (revLookup[input.charCodeAt(stringIndex)] << 2) | (revLookup[input.charCodeAt(stringIndex + 1)] >> 4);
+				output[byteIndex++] = tmp & 0xFF;
+			} else if (placeHolders === 1) {
+				const tmp = (revLookup[input.charCodeAt(stringIndex)] << 10) | (revLookup[input.charCodeAt(stringIndex + 1)] << 4) | (revLookup[input.charCodeAt(stringIndex + 2)] >> 2);
+				output[byteIndex++] = (tmp >> 8) & 0xFF;
+				output[byteIndex++] = tmp & 0xFF;
+			}
+		}
+
+		if (byteIndex < byteLength) {
 			setImmediate(decodeChunk);
 		} else {
-			resolve(Buffer.concat(outputBuffers));
+			resolve(output);
 		}
 	});
 });
